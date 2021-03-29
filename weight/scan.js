4234b481077ccad485b4944c0c91b2ac27a2e7d7
lzt001 <lzt0000001@gmail.com>
2021-03-29 10:55:08 +0800

    add indicator

--- weight/scan.js
+++ weight/scan.js
@@ -146,7 +146,8 @@ function draw_ranges(canvas, height, r) {
         txt = div["obesity"].toFixed(2);
         ctx.rotate(rad_range * (div["obesity"] - div["overweight"]) / weight_range * Math.PI);
         ctx.fillText(txt, -ctx.measureText(txt).width / 2, -canvas.height * 0.415);
-        ctx.rotate((0.5 + start_rad) * Math.PI);
+        //ctx.rotate((0.5 + start_rad) * Math.PI);
+        ctx.setTransform(1, 0, 0, 1, 0, 0);
 
         let bd = 0.0013;
         let line_width = 45;
