99d0fd87214ef8c4c150e07c5e5b2e679e7a87b2
lzt001 <lzt0000001@gmail.com>
2021-03-29 09:59:05 +0800

    add indicator

--- weight/scan.js
+++ weight/scan.js
@@ -130,7 +130,7 @@ function draw_ranges(canvas, height, r) {
         ctx.rotate((2 - 0.5 - rad_range * 0 / weight_range - start_rad) * Math.PI);
         ctx.font = Math.floor(canvas.width / 18) + "px Arial";
         ctx.fillStyle = i_gray;
-        ctx.fillText("0KG", canvas.width / 2, canvas.height / 9);
+        ctx.fillText("0KG", canvas.width / 2, canvas.height * 0.08);
         let img = ctx.getImageData(0, 0, canvas.width, canvas.height);
         ctx.rotate(-(2 - 0.5 - rad_range * 0 / weight_range - start_rad) * Math.PI);
         ctx.putImageData(img, 0, 0);
